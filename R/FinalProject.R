4d3e29c2379e70512ae75916332283a3e6690473
olenaostasheva <oostasheva@middlebury.edu>
2016-04-18 11:01:39 -0400

    git commit -m "Update Final Project"

--- R/FinalProject.R
+++ R/FinalProject.R
@@ -1,9 +1,3 @@
-#Determine unique values
-#we are dealing with 2315 publicly traded companies between April (1998-04-20)  and October(2007-10-30)
-length(unique(x$symbol))
-
-#write function to read these
-
 library(ws.data)
 library(lubridate)
 library(dplyr)
@@ -22,36 +16,50 @@ data(daily.2004)
 data(daily.2005)
 data(daily.2006)
 data(daily.2007)
+data(yearly)
+data(secref)
 
+#Determine unique values
+#we are dealing with 2315 publicly traded companies between April (1998-04-20)  and October(2007-10-30)
+#
+
+#Combine all the data for the years between 1998:2007
 x<-rbind(daily.1998,daily.1999,daily.2000,daily.2001,daily.2002,daily.2003,daily.2004,daily.2005,daily.2006,daily.2007)
 dim(x)
 
-head(r,500)
+#There are 3083 unique public trading companies
+length(unique(x$symbol))
 
-eom <- function(d){
+#Limit number of days to after 1998-10-20 and before 2007-04-30 to allow for (6,6) portfolio
+correct_days<-subset(x,v.date>="1998-10-20" & v.date<="2007-04-30")
+View(correct_days)
 
-        ## Takes a vector of dates and figures out all of last days of the
-        ## month correspoding to each date. Allows us to figure out all
-        ## the dates that we need from an input dataframe instead of
-        ## calculating them ourselves.
 
-        stopifnot(is(d, "Date"))
+#Merge daily with secref data by symbol to know which companies belong to which industries
+#!!!!Question: but the number of unique symbols is different in each set (secref vs correct days)
+y <- tbl_df(secref)
+z <- merge(correct_days, y, by = "symbol")
 
-        ## Trick is to take the date, change it to the first of that
-        ## month, and then add 40 days. The resulting date is guaranteed
-        ## to be in the next month. Then create the first day of that next
-        ## month. Then subtract 1 day. That day is guaranteed to be the
-        ## last day of the month you started in. Hacky but effective.
 
-        next.month   <- as.Date(paste(format(d, "%Y-%m"), "01", sep =
-                                              "-"))          + 40
-        end.of.month <- as.Date(paste(format(next.month, "%Y-%m"), "01",
-                                      sep = "-")) - 1
+#Limit universe to the top 1500 companies on Dec 31st of each year
 
-        invisible(end.of.month)
-}
+y<-mutate(correct_days, year= years(v.date))
 
-eom(x)
+y %>%
+        left_join(y, yearly, by = c("id")) %>%
+        filter(top.1500) %>% tbl_df()
+
+
+z<-tbl_df(correct_days)
+View(z)
+
+z %>%
+        mutate(z, month = months(v.date)) %>%
+        group_by(symbol, month) %>%
+        summarize(ret = sum(tret))
+
+
+
 
 
 head
@@ -65,5 +73,4 @@ require(zoo)
 require(xts)
 '''
 
-x<-rbind()
 
