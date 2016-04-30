4406052abde54f7bf13437ed1d47959852217dcf
olenaostasheva <oostasheva@middlebury.edu>
2016-04-30 11:43:09 -0400

    "updates"

--- R/DataGather_DataAnalysis.R
+++ R/DataGather_DataAnalysis.R
@@ -1,5 +1,4 @@
-
-
+#get rid of this!!!!
 library(ws.data)
 library(lubridate)
 library(dplyr)
@@ -7,13 +6,11 @@ library(tibble)
 library(tidyr)
 library(ggplot2)
 library(RcppRoll)
-
 data(secref)
 data(yearly)
 
 
 #gather the data
-#How can we organize this into one function along with adding secref and yearly
 #Get rid of extremely high prices
 gather.data <- function(symbols, years){
 
@@ -21,27 +18,41 @@ gather.data <- function(symbols, years){
 
         gathered <- data.frame()
 
-        #open up all the daily data files for years
+        #open up all the daily data files for all years
         for( i in years ){
 
                 file.name <- paste("daily", i, sep = ".")
                 data(list = file.name) #create a data list of daily files
 
+        #combine all the daily data in a data frame
                 gathered <- rbind(gathered, subset(eval(parse(text=file.name)), symbol %in% symbols))
         }
 
+        #rename v.date to date, makes more sense
         gathered <- rename(gathered, date = v.date)
 
         #join daily data with secref that contains industry returns
         gathered <- left_join(select(gathered,id,symbol,date,price,tret), select(secref, id, m.ind), by = "id")
 
-        #1) create a new column for Mon-Year variable and 2)attach existing data to yearly
+        #1) create a new column for year and mon-year variable and 2)attach existing data to yearly
         gathered <- left_join(mutate(gathered, year = lubridate::year(date), month = paste(lubridate::month(date, TRUE, TRUE), year, sep = "-")),
                  select(yearly, id, year, top.1500),
                  by = c("year", "id"))
 
-        #make gathered data a tbl_df for opening it
+        #make gathered data a tbl_df
         gathered<-tbl_df(gathered)
+
+        #clean data: get rid of stocks with high returns
+        #decide how to deal with 1) high prices (Berkshire) and 2) high returns
+        #gathered<-filter(gathered, ! symbol %in% c ())
+
+        #find past and forward 6 months returns to be used later in calculations of
+        # MG and JT strategies
+        gathered<-gathered %>% group_by(symbol) %>%
+                mutate(ret.6.0.m=roll_prod(tret+1, 126,fill=NA,align="right")-1) %>%
+                mutate(ret.0.6.m=roll_prod(lead(tret,n=1)+1,126,fill=NA, align="left")-1) %>%
+                ungroup()
+
         invisible(gathered)
 }
 
@@ -49,106 +60,23 @@ gather.data <- function(symbols, years){
 
 #Open and save the data
 x<-gather.data(symbols=secref$symbol,1998:2007)
-View(head(x,100))
-save(x, file="gather.Rdata")
-f<-file.choose()
+View(head(x,1000))
+summary(x)
+
+
 
 #Check whether data makes sense
 #Notice: max(tret) is 499 (no sense), think how to deal with this
 summary(x)
 View(head(x))
+d<-filter(x, tret>2)
+View(d)
+
+max(x$price)
+###JT strategy
 
 
 
-#MG startegy
-#Rank the industry returns using MG function
-#Take care of NAs
-
-MG.Data <- function(data.x) {
-    data.x %>%
-        group_by(m.ind) %>%
-        arrange(m.ind, date) %>% #group and arrange by industry and date within each industry
-        mutate(ind_ret = roll_mean(tret)) %>% #find industry returns by finding the rolling mean of all the stocks in each industry
-        ungroup() %>%
-        mutate(ind_rank = row_number(desc(ind_ret))) %>% #create the column called insutry return
-        select(m.ind, ind_ret, ind_rank) %>% #select the columns to spit out for R
-        arrange(ind_rank) #arrange by industry rank
-}
-
-
-View(MG.Data)
-
-#Function for (6,6) strategy
-#At the beginning of each month (t) stocks are ranked in ascending order according to their industry past performance
-#winner:stocks in the top 30% of industries
-#loosers:stocks in the bottom 30% of industries
-
-
-data.6.0 <- function(data.x) {
-
-        #filter by last day of the month and allow only unique dates
-        anchors<-unique(data.x %>% group_by(month) %>%
-                                filter(min_rank(desc(date)) == 1) %>%
-                                filter(top.1500))
-
-        anchors<-as.Date(data.x$date)
-
-        #inititate the loop to calculate 125 days before for each date
-        for (i in 1:length(anchors)){
-                anchor<-anchors[i]
-
-                date1<-anchor-125 #for each day substract 125 days (~6 months)
-                date1<-as.Date(date1)
-
-                date2<-anchor+0
-                date2<-as.Date(date2)
-        }
-
-        data.x <- filter(data.x$date, date>= date1, date<= date2) #find the 6 months period
-
-        invisible(data.x)
-}
-
-
-#Return for MG
-#30% of top industries --> 21 industries
-#30% of bottom industries --> 21 industries
-
-MG.6.0.ret <- function(data.x) {
-
-        data1 <- data.6.0(data.x)
-
-        #rank the date according to the previous 6 months of performance
-        rank <- MG.Data(data1)
-
-        #filter out 21 best performing industry(30% out of 69 industries)
-        winners <- filter(rank, ind_rank<=21)
-        #filter out 21 worst performing industry
-        losers <- filter(rank, ind_rank>=48)
-
-        data1  %>%
-                select(-top.1500, -id, -year) %>%
-                filter(m.ind %in% winners) %>% #take out all the winner industries
-                group_by(symbol) %>%
-                arrange(symbol, date) %>% #group and arrange by symbol and date
-                mutate(cum_ret = cumprod(1+tret) - 1) -> winner.ret #find cumilative return
-
-        mean(winner.ret$cum_ret) -> winner_portfolio #find the mean of the returns for all the stocks in each winner industry
-
-        #repeat for looser portfolio
-        data1  %>%
-                select(-top.1500, -id, -year) %>%
-                filter(m.ind %in% losers) %>%
-                group_by(symbol) %>%
-                arrange(symbol, date) %>%
-                mutate(cum_ret = cumprod(1+tret) - 1) -> looser.ret
-
-        mean(looser.ret2$cum_ret) -> looser_portfolio
-
-        #give out a table of winner and looser portfolios
-        result <- c(winner_portfolio, looser_portfolio)
-        invisible(result)
-}
 
 
 
@@ -217,7 +145,7 @@ for(i in 1:length(initial.date)) {
 ##Then we can long the winners and short the losers for a period of 6 months.
 ##how to long winnders and short losers in this senario?
 
-
+#################################################
 #Find 52 week high
 #Arrange by symbol and date first
 #How to arrange the code so that we have the last
@@ -249,20 +177,50 @@ stopifnot(
 )
 
 
-#FInd the ratio and arrange in descneding order
+#Find the ratio and arrange in descneding order
 #Arrange the ratios in descenig order for each month
 monthly %>% group_by(date) %>%
       mutate(ratio_52_wh=price/price_52_wh) %>%
-      arrange(desc(ratio_52_wh)) ->m
+      arrange(desc(ratio_52_wh)) %>%
+      mutate(rank_52_wh = row_number(desc(ratio_52_wh))) %>%
+      arrange(rank_52_wh)->m
+
+#Alternative to ranking. What's better?
+monthly %>% group_by(date) %>%
+        mutate(ratio_52_wh=price/price_52_wh) %>%
+        arrange(desc(ratio_52_wh)) %>%
+        mutate(wh_class=ntile(ratio_52_wh, n=3))->instead
+View(instead)
+
+
+#Get rid of NAs first
+#Remove all the rows where 52-week high can't be calculated (= NA)
+m<-m[complete.cases(m),]
 
 #Check whether the size of the ratio makes sense (Max is 1 = good)
 View(m)
 summary(m)
 
+#Form the portfolios
+#Notice: that the # of companies changes each month how to find the top 30% for EACH month. This function just calculates that
+#top 30% is ranks below 711.
+winners<-m %>%
+        group_by(month) %>%
+        filter(rank_52_wh<0.333*max(m$rank_52_wh))
+View(winners)
+
+loosers<-m %>%
+        group_by(month) %>%
+        filter(rank_52_wh>0.666*max(m$rank_52_wh))
+View(loosers)
 
 #Form the portfolios
+#How to get head(30% and tail 30% of the rows in each month?
 
 
+
+
+###########################################################
 #Find monthly returns
 
 #Error: expecting a single value (if use summarize)!
